4fa20ce5682e9e9a330321836207d77383acb055
SharpDevOps10 <td090833@gmail.com>
2024-06-02 23:36:03 +0300

    Updated tests for balancer to support our Db

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
 	. "gopkg.in/check.v1"
 	"net/http"
@@ -16,13 +17,13 @@ type IntegrationSuite struct{}
 var _ = Suite(&IntegrationSuite{})
 
 const baseAddress = "http://balancer:8090"
-const teamName = "@goyda"
+const teamName = "goyda"
 
 var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-type RespBody struct {
+type ResponseStructure struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
@@ -32,41 +33,90 @@ func (s *IntegrationSuite) TestBalancer(c *C) {
 		c.Skip("Integration test is not enabled")
 	}
 
-	// test server1
-	server1, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
-	if err != nil {
-		c.Error(err)
+	servers := make([]string, 3)
+
+	urls := generateUrls(3)
+	for i, url := range urls {
+		servers[i] = getBalancerResponseServer(c, url)
 	}
 
-	server1Header := server1.Header.Get("lb-from")
-	c.Check(server1Header, Equals, "server1:8080")
-
-	// test server2
-	server2, err := client.Get(fmt.Sprintf("%s/check4", baseAddress))
-	if err != nil {
-		c.Error(err)
+	if servers[0] != servers[2] {
+		c.Errorf("Dissimilar servers for the one url: got %s and %s", servers[0], servers[2])
 	}
 
-	server2Header := server2.Header.Get("lb-from")
-	c.Check(server2Header, Equals, "server2:8080")
+	checkResponseStructure(c, teamName)
+}
 
-	// test server3
-	server3, err := client.Get(fmt.Sprintf("%s/check2", baseAddress))
+func checkResponseStructure(c *C, key string) {
+	url := fmt.Sprintf("%s/api/v1/some-data?key=%s", baseAddress, key)
+	resp, err := client.Get(url)
 	if err != nil {
 		c.Error(err)
+		return
 	}
 
-	server3Header := server3.Header.Get("lb-from")
-	c.Check(server3Header, Equals, "server3:8080")
-
-	// test repeated request
-	server1Repeat, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+	var respStructure ResponseStructure
+	err = json.NewDecoder(resp.Body).Decode(&respStructure)
 	if err != nil {
 		c.Error(err)
+		return
 	}
 
-	server1RepeatHeader := server1Repeat.Header.Get("lb-from")
-	c.Check(server1RepeatHeader, Equals, server1Header)
+	if respStructure.Key != key {
+		c.Errorf("Expected %s, got %s", key, respStructure.Key)
+	}
+
+	if respStructure.Value == "" {
+		c.Errorf("Expected a non-empty Value")
+	}
+
+	fmt.Println(respStructure.Value)
+}
+
+func getBalancerResponseServer(c *C, addr string) string {
+	resp, err := client.Get(addr)
+	if err != nil {
+		c.Error(err)
+		return ""
+	}
+
+	defer resp.Body.Close()
+	server := resp.Header.Get("lb-from")
+	if server == "" {
+		c.Errorf("Header was not found in response %s", addr)
+	}
+	return server
+}
+
+func generateUrls(num int) []string {
+	urls := make([]string, num)
+	for i := 0; i < num; i++ {
+		urls[i] = fmt.Sprintf("%s/api/v1/some-data", baseAddress)
+	}
+	return urls
+}
+
+func checkHttpStatusCodes(c *C, key string, expectedStatusCode int) {
+	addr := fmt.Sprintf("%s/api/v1/some-data?key=%s", baseAddress, key)
+	resp, err := client.Get(addr)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	if resp.StatusCode != expectedStatusCode {
+		c.Errorf("Expected status code %d, got %d", expectedStatusCode, resp.StatusCode)
+	}
+
+	resp.Body.Close()
+}
+
+func (s *IntegrationSuite) TestBalancer_InvalidKey(c *C) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		c.Skip("Integration test is not enabled")
+	}
+
+	checkHttpStatusCodes(c, "invalidKey", http.StatusNotFound)
 }
 
 func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
